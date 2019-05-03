a71de7dc65e2c4742989fe94a9b7286ab1696d73
perezlx <perezlx@HGDLAPPEREZLX>
2019-05-03 16:40:12 -0500

    Creation of CineVController and View. And some design of the layout.

--- CineHitss/Controllers/CineVController.cs
+++ CineHitss/Controllers/CineVController.cs
@@ -3,23 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CineHitss.Models;
 
 namespace CineHitss.Controllers
 {
     public class CineVController : Controller
     {
+        CineHitssEntities db = new CineHitssEntities();
+
         public ActionResult IntroView()
         {
-            return View();
+            var ciudades = db.Ciudads.ToList();
+            return View(ciudades);
         }
 
         public ActionResult GetCiudades()
         {
-            CineHitssEntities db = new CineHitssEntities();
             var ciudades = db.Ciudads.ToList();
-            ViewBag.Cities = ciudades;
-            return View("IntroView");
+            
+            return View(ciudades);
         }
 
+        public ActionResult CinesxCiudad(int id)
+        {
+            var cines = db.Cines.Where(c => c.Ciudad_ID == id);
+            return View(cines.ToList());
+        }
+        
     }
 }
---
+++ CineHitss/Helpers/CinesCiudad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CineHitss.Models;
+
+namespace CineHitss.Helpers
+{
+    public class CinesCiudad
+    {
+        CityTheaterModel ctMdl = new CityTheaterModel();
+        
+    }
+}
